7bd6f9f5251ca5dd04a9e389e4dc7c146721889d
Taviani <1995427+taviani@users.noreply.github.com>
2018-11-08 11:45:18 +0100

    modif .gitignore

---
+++ build/precache-manifest.7e105971f4bf6db6c47f5c62df12ea25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9cf83effa5bcdf383947",
+    "url": "/static/js/main.9cf83eff.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "9cf83effa5bcdf383947",
+    "url": "/static/css/main.2ac09da1.chunk.css"
+  },
+  {
+    "revision": "00664532af206540a175af2a30dd88a2",
+    "url": "/index.html"
+  }
+];
